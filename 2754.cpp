f67908e0fecbcfd24cef89c1198ab936883db67e
snowrain10 <rainef@gmail.com>
2019-12-01 22:39:26 +0900

    1st commit!

--- 2754.cpp
+++ 2754.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
-  cout << "Hello World" << "\n";
+  string grade;
+  cin >> grade;
+  double score = 0;
+  if(grade[0] == 'A') score = 4;
+  else if(grade[0] == 'B') score = 3;
+  else if(grade[0] == 'C') score = 2;
+  else if(grade[0] == 'D') score = 1;
+
+  if(grade[1] == '+') score += 0.3;
+  else if(grade[1] = '-') score -= -0.3;
+
+  cout.setf(ios::fixed);
+  cout.setf(ios::showpoint);
+  cout.precision(1);
+  cout << score << "\n";
   return 0;
 }
---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout << "Hello World" << "\n";
+  return 0;
+}
