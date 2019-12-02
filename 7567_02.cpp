dbb215afa603c04a3756564d4e7f5cb9f3db9d13
snowrain <rainef@gamil.com>
2019-12-03 03:30:38 +0900

    그릇, for

--- 7567_02.cpp
+++ 7567_02.cpp
@@ -4,14 +4,14 @@
 #include <cstdio>
 using namespace std;
 
-//그릇의 크기는 50까지
+//그릇의 높이는 3 ~ 50
 char a[51];
 int ans = 10;
 
 int main(){
 	scanf("%s", a);
 
-  //그릇의 크기는 50을 넘을 수 없으므로
+  //그릇의 높이는 50을 넘을 수 없으므로
 	for (int i = 1; i <= 50; i++) {
     //'\n' 등이 들어오면 종료
 		if (a[i] != '(' && a[i] != ')') break;
