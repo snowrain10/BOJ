b1a1ac5cba67424d01535f163d62ba07b35e1ec3
snowrain <rainef@gamil.com>
2019-12-04 03:13:06 +0900

    2nd commit, 틀림

--- 9506_01.cpp
+++ 9506_01.cpp
@@ -12,22 +12,28 @@ int main()
     int n;
     cin >> n;
     if(n == -1) break;
-    int sum = 1;
+    int sum = 1, max = 1;
     for(int i = 2; i < n; i++) {
       if(n%i == 0) {
         f[i] = 1;
         sum += i;
+        max = i;
       }
-      if(n == sum) {
-        cout << n << " = 1 + ";
-        for(int i = 2; i < n; i++) {
-          if(f[i] == 1)
-            cout << f[i] << " + ";
-        }
-      }
-      else
-        cout << n << " is NOT perfect." << "\n";
     }
+    if(n == sum) {
+      cout << n << " = 1 + ";
+      for(int i = 2; i < n; i++) {
+        if(f[i] == 1) {
+          cout << i;
+          if(i != max)
+            cout << " + ";
+          else
+            cout << "\n";
+          }
+      }
+    }
+    else
+      cout << n << " is NOT perfect." << "\n";
   }
   return 0;
 }
