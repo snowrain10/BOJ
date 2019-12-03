955f213a037f7f3f5790c13271be9403a8a6b378
snowrain <rainef@gamil.com>
2019-12-04 02:54:50 +0900

    1st coding

---
+++ 9506_01.cpp
@@ -0,0 +1,33 @@
+//BOJ 9506	약수들의 합
+#include <iostream>
+using namespace std;
+
+int f[100000];
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  while(true) {
+    int n;
+    cin >> n;
+    if(n == -1) break;
+    int sum = 1;
+    for(int i = 2; i < n; i++) {
+      if(n%i == 0) {
+        f[i] = 1;
+        sum += i;
+      }
+      if(n == sum) {
+        cout << n << " = 1 + ";
+        for(int i = 2; i < n; i++) {
+          if(f[i] == 1)
+            cout << f[i] << " + ";
+        }
+      }
+      else
+        cout << n << " is NOT perfect." << "\n";
+    }
+  }
+  return 0;
+}
