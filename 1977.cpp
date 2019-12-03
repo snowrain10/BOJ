7758cf6efd035702a7c7c4f11e420dff85c634de
snowrain <rainef@gamil.com>
2019-12-04 04:41:28 +0900

    1st commit

---
+++ 1977.cpp
@@ -0,0 +1,11 @@
+//BOJ 1977	완전제곱수
+//M과 N이 주어질 때 M이상 N이하의 자연수 중 완전제곱수인 것을 모두 골라 그 합을 구하고 그 중 최솟값을 찾는 프로그램
+#include <iostream>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  return 0;
+}
