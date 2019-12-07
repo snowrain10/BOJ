b5416b11cf8c45a720263f4443a09cc621bf6703
snowrain <rainef@gamil.com>
2019-12-08 06:47:13 +0900

    생일, vector, pair, sort

--- 5635_01.cpp
+++ 5635_01.cpp
@@ -12,16 +12,17 @@ int main()
   cin.tie(0);
   int n;
   cin >> n;
-
+  //                yyyy mm         dd   name
   vector <pair<pair<int, int>, pair<int, string> > > s(n);
 
   for (int i = 0; i < n; i++)
   {
+    //     name                  dd                   mm                   yyyy
     cin >> s[i].second.second >> s[i].second.first >> s[i].first.second >> s[i].first.first;
   }
-
+  //first부터 정렬이 되고 같으면 뒤에 인자(string이면 사전 순)로 정렬됨.
   sort(s.begin(), s.end());
-
+  //      n-1 제일 큰 수 - 나이가 적음         0 제일 작은 수 - 나이가 많음
   cout << s[n-1].second.second << "\n" << s[0].second.second << "\n";
 
   return 0;
