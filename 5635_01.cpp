a3a933f23a09b33e1f9e7a87b4367fc60633042a
snowrain10 <rainef@gmail.com>
2019-12-08 11:43:29 +0900

    생일, vector, pair, sort

--- 5635_01.cpp
+++ 5635_01.cpp
@@ -11,11 +11,16 @@ int main()
   cin.tie(0);
   int n;
   cin >> n;
+  //pair의 속성을 잘 생각해보면
+  //pair를 이중으로 사용
+  //                yyyy mm         dd   name
   vector <pair<pair<int, int>, pair<int, string>>> s(n);
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < n; i++) {
+    //     dd                    mm                   yyyy                 name
     cin >> s[i].second.second >> s[i].second.first >> s[i].first.second >> s[i].first.first;
   }
-  sort(s.begin, s.end);
+  sort(s.begin(), s.end());
+  //      n-1 : 큰 수 - 나이가 적음        0 : 작은 수 - 나이가 많음
   cout << s[n-1].second.second << "\n" << s[0].second.second << "\n";
   return 0;
 }
