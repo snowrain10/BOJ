9b8c06baddaca21f93711539b112b4733b092f20
snowrain10 <48055331+snowrain10@users.noreply.github.com>
2020-02-23 15:42:15 +0900

    2693 | N번째 큰 수 | 3번째 큰 수 : sort 후 a[MAX-3]

--- 2693.cpp
+++ 2693.cpp
@@ -3,22 +3,23 @@
 
 using namespace std;
 
-const int N = 10;
+const int MAX = 10;
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    
     int T;
     cin >> T;
     while(T--) {
         int a[N];
-        for(int i=0;i<N;i++) {
+        for(int i=0;i<MAX;i++) {
             cin >> a[i];
         }
-        sort(a, a+N);
+        sort(a, a+MAX);
         // 배열 A에서 3번째 큰 값을 출력한다.
-        cout << a[N-3] << "\n";
+        cout << a[MAX-3] << "\n";
     }
 
     return 0;
