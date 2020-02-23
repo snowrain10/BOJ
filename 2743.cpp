2f1136258ecdb3bb171158fa2df6a7bd95e39ff0
snowrain10 <48055331+snowrain10@users.noreply.github.com>
2020-02-24 01:30:46 +0900

    2743 | 단어 길이 재기 | <string> str.size
    또는
    <cstring> <string.h>
    char s[101].
    s.strlen();

---
+++ 2743.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string str;
+    cin >> str;
+    cout << str.size() << "\n";
+    return 0;
+}
