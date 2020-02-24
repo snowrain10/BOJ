79f5f3d464b1b472cf4318060414e680d2792dbd
snowrain10 <48055331+snowrain10@users.noreply.github.com>
2020-02-24 21:22:13 +0900

    11721 | 열 개씩 끊어 출력하기
        for (int i = 0; i < len; i++) {
            if (i % 10 == 0)
                cout << "\n";
            cout << str[i];
        }

---
+++ 11721.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string str;
+    cin >> str;
+    
+    int len = str.size();
+    for (int i = 0; i < len; i++) {
+        if (i % 10 == 0)
+            cout << "\n";
+        cout << str[i];
+    }
+    return 0;
+}
