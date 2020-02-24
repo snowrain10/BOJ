a0c86533b9058f93559180bfdee914ed6fcc260e
snowrain10 <48055331+snowrain10@users.noreply.github.com>
2020-02-24 15:44:44 +0900

    11654 | 아스키 코드 | char c => int a ; a는 c의 아스키 코드

---
+++ 11654.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    char c;
+    cin >> c;
+    int a = c;
+    cout << a;
+    return 0;
+}
