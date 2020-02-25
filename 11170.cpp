844c766d54841bd944c3b8da008766d46e183f00
snowrain10 <48055331+snowrain10@users.noreply.github.com>
2020-02-25 19:14:11 +0900

    11170 | 0의 개수 |
            if (n == 0) cnt = 1;
            for (int i = n; i <= m; i++) {
                int j = i;
                while (j) {
                    int k = j % 10; // 1의 자리 확인
                    if (k == 0) cnt++;
                    j /= 10; // 다음 1의 자리를 추출하기 위해
                }
            }

---
+++ 11170.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int cnt = 0;
+        if (n == 0) cnt = 1;
+        for (int i = n; i <= m; i++) {
+            int j = i;
+            while (j) {
+                int k = j % 10; // 1의 자리 확인
+                if (k == 0) cnt++;
+                j /= 10; // 다음 1의 자리를 추출하기 위해
+            }
+        }
+
+        cout << cnt << "\n";
+    }
+    return 0;
+}
+
+/*
+11170
+0의 개수
+
+문제
+N부터 M까지의 수들을 종이에 적었을 때 종이에 적힌 0들을 세는 프로그램을 작성하라.
+
+예를 들어, N, M이 각각 0, 10일 때 0을 세면 0에 하나, 10에 하나가 있으므로 답은 2이다.
+
+입력
+첫 번째 줄에 테스트 케이스의 수 T가 주어진다.
+
+각 줄에는 N과 M이 주어진다.
+
+1 ≤ T ≤ 20
+0 ≤ N ≤ M ≤ 1,000,000
+출력
+각각의 테스트 케이스마다 N부터 M까지의 0의 개수를 출력한다.
+*/
