72b5bb212acc8055aed6e77b7ded0fc756fdce81
snowrain10 <48055331+snowrain10@users.noreply.github.com>
2020-02-26 11:16:11 +0900

    11651 | 좌표 정렬하기 2 | sort(arr, arr + n, comp);
    bool comp(pair<int, int>& a, pair<int, int>& b)
    {
        if (a.second == b.second)
            return a.first < b.first;
        return a.second < b.second;
    }

---
+++ 11651.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+bool comp(pair<int, int>& a, pair<int, int>& b)
+{
+    if (a.second == b.second)
+        return a.first < b.first;
+    return a.second < b.second;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+
+    pair<int, int> arr[100000];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i].first >> arr[i].second;
+    }
+    sort(arr, arr + n, comp);
+    for (int i = 0; i < n; i++)
+        cout << arr[i].first << " " << arr[i].second << "\n";
+    return 0;
+}
