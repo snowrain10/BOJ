f51ee81e7975a32e2d10ceef33104e8a7e9a351b
snowrain10 <48055331+snowrain10@users.noreply.github.com>
2020-02-26 10:46:39 +0900

    10951 | A+B - 4 | while (cin >> a >> b)

--- 10951.cpp
+++ 10951.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main()
 {
-    int a, b;
-    while (scanf("%d %d", &a, &b) != EOF)
-        printf("%d\n", a + b);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    return 0;
+    int a, b;
+    while (cin >> a >> b) {        
+        cout << a + b << "\n";
+    }
 }
