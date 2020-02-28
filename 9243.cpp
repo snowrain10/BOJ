f5123ad6f1c0cd704dbddd70c90942c4ff8bfc2d
snowrain10 <48055331+snowrain10@users.noreply.github.com>
2020-02-28 21:42:20 +0900

    9243 | 파일 완전 삭제 | if ((strA[i] == strB[i]) ^ !(N & 1))
    if ((strA[i] == strB[i]) ^ !(N & 1))
    N & 1 => & : AND => N%2 와 동일
    ^ => XOR
    (각 자리수가 같고 : 1) ^ (!(짝수): 1) => 0 
    (각 자리수가 다르고 : 0) ^ (!(짝수): 1) => 1 => Deletion failed
    (각 자리수가 다르고 : 0) ^ (!(홀수): 0) => 0
    (각 자리수가 같고 : 1) ^ (!(홀수): 0) => 1 => Deletion failed

--- 9243.cpp
+++ 9243.cpp
@@ -7,37 +7,20 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int arr[1000] = {};
     int N;
     cin >> N;
-    
-    string str1;
-    string str2;
-    cin >> str1 >> str2;
-
-    int len = str1.length();
-    bool D = false;
-    for (int i = 0; i < len; i++) {
-        if (N % 2 != 0) {
-            if (str1[i] == '0' && str2[i] == '1' || str1[i] == '1' && str2[i] == '0')
-                D = true;
-            else {
-                D = false;
-                break;
-            }
-        }
-        else {
-            if (str1[i] == '0' && str2[i] == '0' || str1[i] == '1' && str2[i] == '1')
-                D = true;
-            else {
-                D = false;
-                break;
-            }
+    string strA;
+    string strB;
+    cin >> strA >> strB;
+    int flag = 0;
+    for (int i = 0; i < strA.size(); i++) {
+        if ((strA[i] == strB[i]) ^ !(N & 1)) {
+            cout << "Deletion failed" << "\n";
+            flag = 1;
+            break;
         }
     }
-    if (D)
-        cout << "Deletion succeeded";
-    else
-        cout << "Deletion failed";
+    if (flag == 0)
+        cout << "Deletion succeeded" << "\n";
     return 0;
 }
