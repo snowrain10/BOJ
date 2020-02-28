4d99b8c72db883349ed1abdf928d1b9639ee8574
snowrain10 <48055331+snowrain10@users.noreply.github.com>
2020-02-28 13:39:29 +0900

    5598 | 카이사르 암호 | if (str[i] < 'A') str[i] += 26;

--- 5598.cpp
+++ 5598.cpp
@@ -11,14 +11,9 @@ int main()
     cin >> str;
     int len = str.length();
     for (int i = 0; i < len; i++) {
-        if (str[i] >= 'D' && str[i] <= 'Z')
-            str[i] -= 3;
-        else if (str[i] == 'C')
-            str[i] = 'Z';
-        else if (str[i] == 'B')
-            str[i] = 'Y';
-        else if (str[i] =='A')
-            str[i] = 'X';
+        str[i] -= 3;
+        if (str[i] < 'A')
+            str[i] += 26;
     }
     cout << str << "\n";
     return 0;
