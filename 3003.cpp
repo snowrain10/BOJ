9a46f6124f2c0ed7f567f29fe99aef0f8012f78d
snowrain10 <48055331+snowrain10@users.noreply.github.com>
2020-02-29 15:05:12 +0900

    3003 | 킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ 3003.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << 1 - a << " " << 1 - b << " " << 2 - c << " " << 2 - d << " "
+        << 2 - e << " " << 8 - f;
+    return 0;
+}
